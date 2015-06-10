eb0e6c86863d80a83eae5f7f02f8443bc83fc399
Scott Barstow <sbarstow@gmail.com>
2015-06-10 16:42:07 -0400

    fixed tests and added coverage report

--- test/index.js
+++ test/index.js
@@ -1,20 +1,20 @@
 'use strict'
 
-var Face = require('../index.js');
+var Oxford = require('../index.js');
 
 var assert = require('assert');
 var should = require('should');
 
 describe('init', function(){
-  var primary_key = "ABCDEFGHIJKLMN";
+  var primaryKey = "ABCDEFGHIJKLMN";
 
   it('not set primary key', function(){
-    var face = new Face();
+    var face = new Oxford.Face()
     assert.equal(face.API_PRIMARY_KEY, undefined);
   })
 
   it('set primary key', function(){
-    var face = new Face(primary_key);
-    face.API_PRIMARY_KEY.should.equal(primary_key);
+    var face = new Oxford.Face(primaryKey);
+    face.API_PRIMARY_KEY.should.equal(primaryKey);
   })
 })
